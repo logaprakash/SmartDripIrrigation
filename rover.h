45aec6253acd09b2bca948c9d989ca0672e5039e
logaprakash <logaprakash98@gmail.com>
2016-12-29 09:39:55 +0530

    rover code added
    rover.h have the functions to control rover (3-pin control)

---
+++ rover.h
@@ -0,0 +1,31 @@
+/* pin3 for start and stop of rover (1 for action mode and 0 for rest mode)
+
+   11 - forward
+   10 - left
+   01 - right
+   00 - backward
+
+*/
+
+int pin1,pin2,pin3;
+
+void motor_init(){
+  
+}
+
+void forward(int duration){
+  
+}
+
+void backward(int duration){
+  
+}
+
+void left(int duration){
+  
+}
+
+void right(int duration){
+  
+}
+
