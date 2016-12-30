1e075ce536ac62d9040288b7e4af388494161206
logaprakash <logaprakash98@gmail.com>
2016-12-30 10:15:07 +0530

    getting current date and time from google
    Api call - getTime() funtion to get current time

---
+++ WebMos D1/time.h
@@ -0,0 +1,34 @@
+#include "wifi.h";
+
+String getTime() {
+  Serial.begin(115200);
+  WiFiClient client;
+  while (!client.connect("google.com", 80)) {
+    Serial.println("connection failed, retrying...");
+  }
+
+  client.print("HEAD / HTTP/1.1\r\n\r\n");
+ 
+  while(!client.available()) {
+     yield();
+  }
+
+  while(client.available()){
+    if (client.read() == '\n') {   
+      if (client.read() == 'D') {   
+        if (client.read() == 'a') {   
+          if (client.read() == 't') {   
+            if (client.read() == 'e') {   
+              if (client.read() == ':') {   
+                client.read();
+                String theDate = client.readStringUntil('\r');
+                client.stop();
+                return theDate;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
