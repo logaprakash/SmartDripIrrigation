504c8bee4396459ad29ba940648691109d1788c6
logaprakash <logaprakash98@gmail.com>
2017-01-19 19:36:58 +0530

    Updated

--- Android/app/src/main/java/alpha/smartdripirrigation/simulate.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/simulate.java
@@ -1,7 +1,12 @@
 package alpha.smartdripirrigation;
 
+
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 
 public class simulate extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class simulate extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulate);
+
+        //Enable Back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    //Back button function
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
+
