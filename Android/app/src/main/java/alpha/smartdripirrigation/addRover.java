d7f4db7d6ffa91d08456f577f5c598913eb1d304
logaprakash <logaprakash98@gmail.com>
2017-01-22 19:22:29 +0530

    Filter added for ip

--- Android/app/src/main/java/alpha/smartdripirrigation/addRover.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/addRover.java
@@ -2,14 +2,47 @@ package alpha.smartdripirrigation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class addRover extends AppCompatActivity {
-
+    private EditText ip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_rover);
+        ip = (EditText)findViewById(R.id.ip);
+
+        //Adding filter for ip
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end,
+                                       android.text.Spanned dest, int dstart, int dend) {
+                if (end > start) {
+                    String destTxt = dest.toString();
+                    String resultingTxt = destTxt.substring(0, dstart)
+                            + source.subSequence(start, end)
+                            + destTxt.substring(dend);
+                    if (!resultingTxt
+                            .matches("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
+                        return "";
+                    } else {
+                        String[] splits = resultingTxt.split("\\.");
+                        for (int i = 0; i < splits.length; i++) {
+                            if (Integer.valueOf(splits[i]) > 255) {
+                                return "";
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+
+        };
+        ip.setFilters(filters);
+
         //Enable Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
