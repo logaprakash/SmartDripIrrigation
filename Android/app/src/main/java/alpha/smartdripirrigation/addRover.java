2c5aa8bc7fa1dd79a8e4d200e616e3fd34367f96
logaprakash <logaprakash98@gmail.com>
2017-01-22 18:31:07 +0530

    Add rover page added

---
+++ Android/app/src/main/java/alpha/smartdripirrigation/addRover.java
@@ -0,0 +1,30 @@
+package alpha.smartdripirrigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class addRover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_rover);
+        //Enable Back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Back button function
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+}
