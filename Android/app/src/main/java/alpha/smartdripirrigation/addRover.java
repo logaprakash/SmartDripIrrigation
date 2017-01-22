5dd12147a482c6cf66a38adcca114656cdbf35bd
logaprakash <logaprakash98@gmail.com>
2017-01-22 19:51:48 +0530

    Storing data locally

--- Android/app/src/main/java/alpha/smartdripirrigation/addRover.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/addRover.java
@@ -1,18 +1,32 @@
 package alpha.smartdripirrigation;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class addRover extends AppCompatActivity {
-    private EditText ip;
+    private EditText ip,roverName;
+    private Button addRoverBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_rover);
         ip = (EditText)findViewById(R.id.ip);
+        roverName = (EditText)findViewById(R.id.roverName);
+        addRoverBtn = (Button)findViewById(R.id.addRoverBtn);
 
         //Adding filter for ip
         InputFilter[] filters = new InputFilter[1];
@@ -45,6 +59,46 @@ public class addRover extends AppCompatActivity {
 
         //Enable Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Button function
+        addRoverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String filename = "rovers";
+                String string = roverName.getText().toString() + "$" +ip.getText().toString() + "\n"  ;
+                FileOutputStream outputStream;
+
+                try {
+                    outputStream = openFileOutput(filename, MODE_APPEND);
+                    outputStream.write(string.getBytes());
+                    outputStream.close();
+                    File file = new File(getApplicationContext().getFilesDir(), filename);
+
+                    //Read text from file
+                    StringBuilder text = new StringBuilder();
+
+                    try {
+                        BufferedReader br = new BufferedReader(new FileReader(file));
+                        String line;
+
+                        while ((line = br.readLine()) != null) {
+                            String[] separated = line.split("\\$");
+                            Log.e("Name:",separated[0]);
+                            Log.e("IP:",separated[1]);
+                        }
+                        br.close();
+                    }
+                    catch (IOException e) {
+                        //You'll need to add proper error handling here
+                    }
+
+                    onBackPressed();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
     //Back button function
