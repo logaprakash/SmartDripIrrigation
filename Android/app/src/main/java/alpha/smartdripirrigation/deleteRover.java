b1480435c742172ed3c76f856a5dd673995a92f9
logaprakash <logaprakash98@gmail.com>
2017-01-23 14:57:08 +0530

    Delete rover page added

---
+++ Android/app/src/main/java/alpha/smartdripirrigation/deleteRover.java
@@ -0,0 +1,13 @@
+package alpha.smartdripirrigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class deleteRover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_rover);
+    }
+}
