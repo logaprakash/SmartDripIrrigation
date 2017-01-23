c77c22215fb9ac48d52e91f6823b7994461e9021
logaprakash <logaprakash98@gmail.com>
2017-01-23 15:01:12 +0530

    rover List added in view rover page

--- Android/app/src/main/java/alpha/smartdripirrigation/viewRover.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/viewRover.java
@@ -2,12 +2,31 @@ package alpha.smartdripirrigation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class viewRover extends AppCompatActivity {
+
+    String[] roverNames = {
+            "Rover-1",
+            "Rover-2",
+            "Rover-3",
+            "Rover-4",
+            "Rover-5",
+            "Rover-6",
+            "Rover-7",
+            "Rover-8"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_rover);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                R.layout.rover_list, roverNames);
+
+        ListView listView = (ListView) findViewById(R.id.roverList);
+        listView.setAdapter(adapter);
     }
 }
