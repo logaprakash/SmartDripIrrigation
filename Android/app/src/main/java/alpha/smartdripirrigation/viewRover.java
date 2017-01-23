548dac649333806c7140d8fb7de8f0f7be3980e2
logaprakash <logaprakash98@gmail.com>
2017-01-23 14:52:24 +0530

    View rover page added

---
+++ Android/app/src/main/java/alpha/smartdripirrigation/viewRover.java
@@ -0,0 +1,13 @@
+package alpha.smartdripirrigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class viewRover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_rover);
+    }
+}
