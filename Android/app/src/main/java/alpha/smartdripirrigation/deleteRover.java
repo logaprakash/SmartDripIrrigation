79737249b60342d2fdb50b8563f044f0fa57c6f5
logaprakash <logaprakash98@gmail.com>
2017-01-23 15:12:13 +0530

    Back button added in delete rover page

--- Android/app/src/main/java/alpha/smartdripirrigation/deleteRover.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/deleteRover.java
@@ -2,6 +2,7 @@ package alpha.smartdripirrigation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class deleteRover extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class deleteRover extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete_rover);
+        //Enable Back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //Back button function
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
