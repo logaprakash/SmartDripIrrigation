a50d89dbc05daafc9716d5bb4c7808e7dd7e7619
logaprakash <logaprakash98@gmail.com>
2017-01-24 18:48:29 +0530

    filter function added

--- Android/app/src/main/java/alpha/smartdripirrigation/addRover.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/addRover.java
@@ -31,33 +31,7 @@ public class addRover extends AppCompatActivity {
         addRoverBtn = (Button)findViewById(R.id.addRoverBtn);
 
         //Adding filter for ip
-        InputFilter[] filters = new InputFilter[1];
-        filters[0] = new InputFilter() {
-            @Override
-            public CharSequence filter(CharSequence source, int start, int end,
-                                       android.text.Spanned dest, int dstart, int dend) {
-                if (end > start) {
-                    String destTxt = dest.toString();
-                    String resultingTxt = destTxt.substring(0, dstart)
-                            + source.subSequence(start, end)
-                            + destTxt.substring(dend);
-                    if (!resultingTxt
-                            .matches("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
-                        return "";
-                    } else {
-                        String[] splits = resultingTxt.split("\\.");
-                        for (int i = 0; i < splits.length; i++) {
-                            if (Integer.valueOf(splits[i]) > 255) {
-                                return "";
-                            }
-                        }
-                    }
-                }
-                return null;
-            }
-
-        };
-        ip.setFilters(filters);
+        filter();
 
         //Enable Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -175,5 +149,34 @@ public class addRover extends AppCompatActivity {
         }
     }
 
+    private void filter(){
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end,
+                                       android.text.Spanned dest, int dstart, int dend) {
+                if (end > start) {
+                    String destTxt = dest.toString();
+                    String resultingTxt = destTxt.substring(0, dstart)
+                            + source.subSequence(start, end)
+                            + destTxt.substring(dend);
+                    if (!resultingTxt
+                            .matches("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
+                        return "";
+                    } else {
+                        String[] splits = resultingTxt.split("\\.");
+                        for (int i = 0; i < splits.length; i++) {
+                            if (Integer.valueOf(splits[i]) > 255) {
+                                return "";
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+
+        };
+        ip.setFilters(filters);
+    }
 
 }
