2d7f05efb58deb76be29f37e8c3c898d1d683601
logaprakash <logaprakash98@gmail.com>
2017-01-25 14:03:28 +0530

    added filter

--- Android/app/src/main/java/alpha/smartdripirrigation/deleteRover.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/deleteRover.java
@@ -2,6 +2,7 @@ package alpha.smartdripirrigation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +21,8 @@ public class deleteRover extends AppCompatActivity {
         clearAllBtn = (Button)findViewById(R.id.clearAllBtn);
         deleteRoverBtn = (Button)findViewById(R.id.deleteRoverBtn);
         ip = (EditText)findViewById(R.id.deleteIP);
+
+        ipFilter();
 
         //Enable Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -50,5 +53,36 @@ public class deleteRover extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    //IP TEXT BOX FILTERING
+    private void ipFilter(){
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end,
+                                       android.text.Spanned dest, int dstart, int dend) {
+                if (end > start) {
+                    String destTxt = dest.toString();
+                    String resultingTxt = destTxt.substring(0, dstart)
+                            + source.subSequence(start, end)
+                            + destTxt.substring(dend);
+                    if (!resultingTxt
+                            .matches("^\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
+                        return "";
+                    } else {
+                        String[] splits = resultingTxt.split("\\.");
+                        for (int i = 0; i < splits.length; i++) {
+                            if (Integer.valueOf(splits[i]) > 255) {
+                                return "";
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+
+        };
+        ip.setFilters(filters);
     }
 }
