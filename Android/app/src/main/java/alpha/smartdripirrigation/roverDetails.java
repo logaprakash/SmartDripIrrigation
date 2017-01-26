b3c921b762f0852d4ff1402b79db241213f3cd3a
logaprakash <logaprakash98@gmail.com>
2017-01-26 14:47:31 +0530

    added back button

--- Android/app/src/main/java/alpha/smartdripirrigation/roverDetails.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/roverDetails.java
@@ -2,6 +2,7 @@ package alpha.smartdripirrigation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class roverDetails extends AppCompatActivity {
 
@@ -9,6 +10,22 @@ public class roverDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rover_details);
+
+        //Enable Back button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         
     }
+
+    //Back button function
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
