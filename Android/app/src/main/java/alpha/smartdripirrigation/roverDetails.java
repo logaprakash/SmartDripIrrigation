2f98cc0bb581ec019198d7cf0246abc8cf2de54e
logaprakash <logaprakash98@gmail.com>
2017-01-27 12:30:22 +0530

    Make active rover added

--- Android/app/src/main/java/alpha/smartdripirrigation/roverDetails.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/roverDetails.java
@@ -1,12 +1,17 @@
 package alpha.smartdripirrigation;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class roverDetails extends AppCompatActivity {
     TextView name , ip;
+    Button activeRoverBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,15 +19,29 @@ public class roverDetails extends AppCompatActivity {
         setContentView(R.layout.activity_rover_details);
         name =  (TextView)findViewById(R.id.nameDetails);
         ip =  (TextView)findViewById(R.id.ipDetails);
-
+        activeRoverBtn = (Button)findViewById(R.id.activeRoverBtn);
         //Enable Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Bundle bundle = getIntent().getExtras();
         String temp = bundle.getString("message");
-        String[] separated = temp.split("\\n");
-        name.setText(separated[0]);
-        ip.setText(separated[1]);
+        final String[] separated1 = temp.split("\\n");
+        name.setText(separated1[0]);
+        ip.setText(separated1[1]);
+
+        activeRoverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] separated = separated1[0].split(":");
+                String value = separated[1].trim();
+                SharedPreferences sharedPref = getApplication().getSharedPreferences(value,0);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString(getString(R.string.active_rover), value);
+                editor.commit();
+                String temp2 = sharedPref.getString(getString(R.string.active_rover), "none");
+                Toast.makeText(getApplicationContext(),temp2,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     //Back button function
