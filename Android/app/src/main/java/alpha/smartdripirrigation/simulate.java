cfe360b4f933268c44341bc8eedb6994f08a507b
logaprakash <logaprakash98@gmail.com>
2017-01-28 08:20:24 +0530

    wifi added

--- Android/app/src/main/java/alpha/smartdripirrigation/simulate.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/simulate.java
@@ -2,21 +2,39 @@ package alpha.smartdripirrigation;
 
 
 
+import android.content.BroadcastReceiver;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 
+
 public class simulate extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simulate);
-
+        WifiManager wifiManager=(WifiManager)getSystemService(WIFI_SERVICE);
         //Enable Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        wifiManager.setWifiEnabled(true);
+        WifiConfiguration wifiConfig = new WifiConfiguration();
+
+        wifiConfig.SSID = String.format("\"%s\"", "NU-Student-3");
+        wifiConfig.preSharedKey = String.format("\"%s\"", "123443211");
+
+
+        int netId = wifiManager.addNetwork(wifiConfig);
+        wifiManager.disconnect();
+        wifiManager.enableNetwork(netId, true);
+        wifiManager.reconnect();
     }
 
     //Back button function
