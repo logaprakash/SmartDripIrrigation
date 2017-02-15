b6a0643499968d01263c36729b89421bb831655b
logaprakash <logaprakash98@gmail.com>
2017-02-15 23:14:38 +0530

    added

---
+++ WeMos D1/wifi.h
@@ -0,0 +1,55 @@
+#include <ESP8266WiFi.h>
+WiFiClient client;
+WiFiServer server(80);
+
+void initWifi(char* ssid, char* password) {
+   Serial.begin(115200);
+   Serial.print("Connecting to ");
+   Serial.print(ssid);
+   WiFi.begin(ssid, password);
+   
+
+   while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+   }
+  Serial.print("\nWiFi connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(500);
+}
+
+void server_begin(){
+  server.begin();
+}
+
+IPAddress local_ip(){
+  return WiFi.localIP();
+  }
+  
+void client_available(){
+  client = server.available();
+  while(!client){
+  }
+}
+
+void client_data_available(){
+  while(!client.available()){
+    delay(1);
+  }
+}
+
+String client_request(){
+ return client.readStringUntil('\r'); 
+}
+
+void client_flush(){
+  client.flush();
+}
+
+void client_print(String msg){
+  client.print(msg);
+}
+void client_println(String msg){
+  client.println(msg);
+}
+
