41a22ee4daa01dd8ca2138c613abc090d5e800f4
logaprakash <logaprakash98@gmail.com>
2017-03-19 21:45:14 +0530

    Update

---
+++ WeMos D1/index/blob.h
@@ -0,0 +1,103 @@
+
+const char* host = "cyberknights.in";
+String getBlob(String container,String segment){
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return "Failed";
+  }
+
+  // We now create a URI for the request
+  String url = "/api/sdi/getBlobContent.php?name="+container+"&segment="+segment;
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+               
+  int timeout = millis() + 5000;
+  while (client.available() == 0) {
+    if (timeout - millis() < 0) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return "Failed";
+    }
+  }
+  int count = 0;
+  String result;
+  while(client.available()) {
+    String line = client.readStringUntil('\r');
+    count++;
+    if(count>10)
+      result = line;
+  } 
+  result.trim();
+  Serial.println("Response: "+ result);
+
+  
+
+  Serial.println();
+  Serial.println("closing connection");
+  return result;
+  
+  }
+
+void updateBlob(String container,String segment,String content){
+
+  Serial.print("connecting to ");
+  Serial.println(host);
+
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  // We now create a URI for the request
+  String url = "/api/sdi/updateBlob.php?name="+container+"&segment="+segment+"&content="+content;
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+               
+  int timeout = millis() + 5000;
+  while (client.available() == 0) {
+    if (timeout - millis() < 0) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return;
+    }
+  }
+  int count = 0;
+  String result;
+  while(client.available()) {
+    String line = client.readStringUntil('\r');
+    count++;
+    if(count>10)
+      result = line;
+  } 
+  result.trim();
+  Serial.println("Response: "+ result);
+
+  
+
+  Serial.println();
+  Serial.println("closing connection");
+  return;
+  
+  }
