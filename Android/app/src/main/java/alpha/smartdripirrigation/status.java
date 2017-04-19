40ae468764598800ed077fcaf6356d890c56f401
logaprakash <logaprakash98@gmail.com>
2017-04-19 20:59:00 +0530

    status

--- Android/app/src/main/java/alpha/smartdripirrigation/status.java
+++ Android/app/src/main/java/alpha/smartdripirrigation/status.java
@@ -1,5 +1,6 @@
 package alpha.smartdripirrigation;
 
+import android.app.ProgressDialog;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -7,16 +8,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.InputStream;
 
 public class status extends AppCompatActivity {
+    ProgressDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status);
+        dialog = ProgressDialog.show(status.this, "",
+                "Getting segment status...", true);
         new DownloadImageTask((ImageView) findViewById(R.id.imgview))
-                .execute("http://smartdripirrigation.blob.core.windows.net/sam/parallax.jpg");
+                .execute("http://smartdripirrigation.blob.core.windows.net/sam/work1.jpg");
 
     }
 
@@ -41,7 +47,22 @@ public class status extends AppCompatActivity {
         }
 
         protected void onPostExecute(Bitmap result) {
+            try {
+                int count = 0;
+                int height = result.getHeight();
+                int width = result.getWidth();
+                int length = width * height;
+                int[] pixels = new int[length];
+                result.getPixels(pixels, 0, width, 0, 0, width, height);
+                for (int i = 0; i < pixels.length; i++) {
+                        if ((pixels[i] & 0x00FFFFFF) == 0)
+                            count++;
+                }
+                Toast.makeText(getApplicationContext(), String.valueOf(count), Toast.LENGTH_LONG).show();
+            }
+            catch (Exception e){ Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();}
             bmImage.setImageBitmap(result);
+            dialog.dismiss();
         }
     }
 }
