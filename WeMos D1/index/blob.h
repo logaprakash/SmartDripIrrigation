b081804731b2fcb222326594ae350598b8908a9c
logaprakash <logaprakash98@gmail.com>
2017-04-21 00:08:41 +0530

    Added checkTime function

--- WeMos D1/index/blob.h
+++ WeMos D1/index/blob.h
@@ -50,7 +50,56 @@ String getBlob(String container,String segment){
   return result;
   
   }
+String checkTime(String parameter){
+  
+  Serial.print("connecting to ");
+  Serial.println(host);
 
+  // Use WiFiClient class to create TCP connections
+  WiFiClient client;
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return "Failed";
+  }
+
+  // We now create a URI for the request
+  String url = "/api/sdi/checkTime.php?parameter="+parameter;
+
+  Serial.print("Requesting URL: ");
+  Serial.println(url);
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" + 
+               "Connection: close\r\n\r\n");
+               
+  int timeout = millis() + 5000;
+  while (client.available() == 0) {
+    if (timeout - millis() < 0) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      return "Failed";
+    }
+  }
+  int count = 0;
+  String result;
+  while(client.available()) {
+    
+    String line = client.readStringUntil('\r');
+    count++;
+    if(count>10)
+      result = line;
+  } 
+  result.trim();
+  Serial.println("Response: "+ result);
+
+  
+
+  Serial.println();
+  Serial.println("closing connection");
+  return result;
+  }
 void updateBlob(String container,String segment,String content){
 
   Serial.print("connecting to ");
